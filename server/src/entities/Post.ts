47c222372d0a1e6347aa53043c13f21a4e143279
Casper Feng <casp.feng@gmail.com>
2020-08-23 11:05:02 +0200

    add post migration

--- server/src/entities/Post.ts
+++ server/src/entities/Post.ts
@@ -5,12 +5,12 @@ export class Post {
   @PrimaryKey()
   id!: number;
 
-  @Property()
+  @Property({ type: 'date' })
   createdAt = new Date();
 
-  @Property({ onUpdate: () => new Date() })
+  @Property({ type: 'date', onUpdate: () => new Date() })
   updatedAt = new Date();
 
-  @Property()
+  @Property({ type: 'text' })
   title!: string;
 }
--- server/src/index.ts
+++ server/src/index.ts
@@ -1,12 +1,9 @@
 import { MikroORM } from '@mikro-orm/core';
 import microConfig from './mikro-orm.config';
-import { Post } from './entities/Post';
 
 const main = async () => {
   const orm = await MikroORM.init(microConfig);
-
-  const post = orm.em.create(Post, { title: 'My first post' });
-  console.log(post);
+  await orm.getMigrator().up();
 };
 
 main().catch((err) => console.error(err));
