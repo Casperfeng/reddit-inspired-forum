c2157058717416db49b4b8e0c4e6467077cff6a6
Casper Feng <casp.feng@gmail.com>
2020-08-23 20:39:25 +0200

    add graphql example endpoint

--- server/src/index.ts
+++ server/src/index.ts
@@ -1,13 +1,28 @@
+import 'reflect-metadata';
 import express from 'express';
+import { ApolloServer } from 'apollo-server-express';
+import { buildSchema } from 'type-graphql';
+import { HelloResolver } from './resolvers/hello';
 import { MikroORM } from '@mikro-orm/core';
 import microConfig from './mikro-orm.config';
 
 const main = async () => {
   const orm = await MikroORM.init(microConfig);
   await orm.getMigrator().up();
+
   const app = express();
+
+  const apolloServer = new ApolloServer({
+    schema: await buildSchema({
+      resolvers: [HelloResolver],
+      validate: false,
+    }),
+  });
+
+  apolloServer.applyMiddleware({ app });
+
   app.listen(8000, () => {
-    console.log('Server is running on localhost:8000');
+    console.log('server started on localhost:8000');
   });
 };
 
