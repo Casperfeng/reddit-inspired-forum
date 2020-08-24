c4dda6961ce56c4cb9b1877d2ceb1de506545ede
Casper Feng <casp.feng@gmail.com>
2020-08-24 13:33:37 +0200

    add get posts by id

--- server/src/resolvers/post.ts
+++ server/src/resolvers/post.ts
@@ -1,4 +1,4 @@
-import { Resolver, Query, Ctx } from 'type-graphql';
+import { Resolver, Query, Ctx, Arg, Int, Mutation } from 'type-graphql';
 import { Post } from '../entities/Post';
 import { MyContext } from '../types';
 
@@ -7,5 +7,23 @@ export class PostResolver {
   @Query(() => [Post])
   posts(@Ctx() { em }: MyContext): Promise<Post[]> {
     return em.find(Post, {});
+  }
+
+  @Query(() => Post, { nullable: true })
+  post(
+    @Arg('id', () => Int) id: number,
+    @Ctx() { em }: MyContext
+  ): Promise<Post | null> {
+    return em.findOne(Post, { id });
+  }
+
+  @Mutation(() => Post, { nullable: true })
+  async createPost(
+    @Arg('title', () => String) title: string,
+    @Ctx() { em }: MyContext
+  ): Promise<Post | null> {
+    const post = em.create(Post, { title });
+    await em.persistAndFlush(post);
+    return post;
   }
 }
