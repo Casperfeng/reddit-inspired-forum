767c1eb9f15d0a620f42bd44f29f20355ca50a3a
Casper Feng <casp.feng@gmail.com>
2020-08-25 09:18:48 +0200

    add user entity

---
+++ server/src/entities/User.ts
@@ -0,0 +1,25 @@
+import { Entity, PrimaryKey, Property } from '@mikro-orm/core';
+import { ObjectType, Field, Int } from 'type-graphql';
+
+@ObjectType()
+@Entity()
+export class User {
+  @Field(() => Int)
+  @PrimaryKey()
+  id!: number;
+
+  @Field(() => String)
+  @Property({ type: 'date' })
+  createdAt = new Date();
+
+  @Field(() => String)
+  @Property({ type: 'date', onUpdate: () => new Date() })
+  updatedAt = new Date();
+
+  @Field(() => String)
+  @Property({ type: 'text', unique: true })
+  username!: string;
+
+  @Property({ type: 'text' })
+  password!: string;
+}
