5032d9830cbb6488301cdde8f023267060339bac
Casper Feng <casp.feng@gmail.com>
2020-08-25 15:09:47 +0200

    add redis config

--- server/src/index.ts
+++ server/src/index.ts
@@ -8,18 +8,45 @@ import { UserResolver } from './resolvers/user';
 import { MikroORM } from '@mikro-orm/core';
 import microConfig from './mikro-orm.config';
 
+import redis from 'redis';
+import session from 'express-session';
+import connectRedis from 'connect-redis';
+import { isProd } from './constants';
+
 const main = async () => {
   const orm = await MikroORM.init(microConfig);
   await orm.getMigrator().up();
 
   const app = express();
 
+  const RedisStore = connectRedis(session);
+  const redisClient = redis.createClient();
+
+  app.use(
+    session({
+      name: 'qid',
+      store: new RedisStore({
+        client: redisClient,
+        disableTouch: true,
+      }),
+      cookie: {
+        maxAge: 1000 * 60 * 60 * 24 * 365 * 10, //10 years
+        httpOnly: true,
+        sameSite: 'lax', // related to csrf protection
+        secure: isProd, //only works in https
+      },
+      //TODO hide secret in .env
+      secret: 'dsjaiodjisaodjijdiowqdjsia',
+      resave: false,
+    })
+  );
+
   const apolloServer = new ApolloServer({
     schema: await buildSchema({
       resolvers: [HelloResolver, PostResolver, UserResolver],
       validate: false,
     }),
-    context: () => ({ em: orm.em }),
+    context: ({ req, res }) => ({ em: orm.em, req, res }),
   });
 
   apolloServer.applyMiddleware({ app });
