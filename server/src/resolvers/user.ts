f054685941f42ce95ad8ce28b7d5fa564409737c
Casper Feng <casp.feng@gmail.com>
2020-08-25 11:58:36 +0200

    add login with field errors

--- server/src/resolvers/user.ts
+++ server/src/resolvers/user.ts
@@ -1,4 +1,12 @@
-import { Resolver, Mutation, Arg, InputType, Field, Ctx } from 'type-graphql';
+import {
+  Resolver,
+  Mutation,
+  Arg,
+  InputType,
+  Field,
+  Ctx,
+  ObjectType,
+} from 'type-graphql';
 import { MyContext } from 'src/types';
 import { User } from '../entities/User';
 import argon2 from 'argon2';
@@ -11,19 +19,65 @@ class UsernamePasswordInput {
   password: string;
 }
 
+@ObjectType()
+class FieldError {
+  @Field()
+  field: string;
+
+  @Field()
+  message: string;
+}
+
+@ObjectType()
+class UserResponse {
+  @Field(() => [FieldError], { nullable: true })
+  errors?: FieldError[];
+
+  @Field()
+  user?: User;
+}
+
 @Resolver()
 export class UserResolver {
-  @Mutation(() => User)
+  @Mutation(() => UserResponse)
   async register(
     @Arg('options', () => UsernamePasswordInput) options: UsernamePasswordInput,
     @Ctx() { em }: MyContext
-  ): Promise<User> {
+  ): Promise<UserResponse> {
     const hashedPwd = await argon2.hash(options.password);
     const user = await em.create(User, {
-      username: options.username,
+      username: options.username.toLowerCase(),
       password: hashedPwd,
     });
     await em.persistAndFlush(user);
-    return user;
+    return { user };
+  }
+
+  @Mutation(() => UserResponse, { nullable: true })
+  async login(
+    @Arg('options', () => UsernamePasswordInput) options: UsernamePasswordInput,
+    @Ctx() { em }: MyContext
+  ): Promise<UserResponse> {
+    const user = await em.findOne(User, {
+      username: options.username.toLowerCase(),
+    });
+
+    if (!user) {
+      return {
+        errors: [
+          {
+            field: 'username',
+            message: 'username does not exist',
+          },
+        ],
+      };
+    }
+    const hashedPwd = await argon2.hash(options.password);
+    const valid = await argon2.verify(hashedPwd, options.password);
+
+    if (!valid) {
+      return { errors: [{ field: 'password', message: 'incorrect password' }] };
+    }
+    return { user };
   }
 }
