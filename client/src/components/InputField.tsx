6a01104f0863bc7e77f6f3e9028c9dcc6e23c03d
Casper Feng <casp.feng@gmail.com>
2020-08-26 22:02:47 +0200

    remove uneeded prop

--- client/src/components/InputField.tsx
+++ client/src/components/InputField.tsx
@@ -10,14 +10,13 @@ import { useField } from 'formik';
 type Props = InputHTMLAttributes<HTMLInputElement> & {
   name: string;
   label: string;
-  placeholder: string;
 };
 
-const InputField: React.FC<Props> = (props) => {
+const InputField: React.FC<Props> = ({ label, size: _, ...props }) => {
   const [field, { error }] = useField(props);
   return (
     <FormControl isInvalid={!!error}>
-      <FormLabel htmlFor={props.name}>{props.label}</FormLabel>
+      <FormLabel htmlFor={props.name}>{label}</FormLabel>
       <Input {...field} id={props.name} placeholder={props.placeholder} />
       {error && <FormErrorMessage>{error}</FormErrorMessage>}
     </FormControl>
