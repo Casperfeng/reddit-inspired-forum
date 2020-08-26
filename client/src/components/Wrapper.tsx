70628e84f0723f545a36b2a55ab072f90e811ad3
Casper Feng <casp.feng@gmail.com>
2020-08-26 22:10:06 +0200

    add mt to wrapper component

--- client/src/components/Wrapper.tsx
+++ client/src/components/Wrapper.tsx
@@ -2,12 +2,17 @@ import React from 'react';
 import { Box } from '@chakra-ui/core';
 
 interface Props {
-  size: 'small' | 'default';
+  size?: 'small' | 'default';
 }
 
 const Wrapper: React.FC<Props> = ({ children, size = 'default' }) => {
   return (
-    <Box maxW={size === 'default' ? '850px' : '450px'} w='100%' mx='auto'>
+    <Box
+      mt={7}
+      maxW={size === 'default' ? '850px' : '450px'}
+      w='100%'
+      mx='auto'
+    >
       {children}
     </Box>
   );
