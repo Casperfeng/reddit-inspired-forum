33a9e1f5a3081a1ca9d8e970ff08e087417534b1
Casper Feng <casp.feng@gmail.com>
2020-08-30 19:40:40 +0200

    add navbar to register page

--- client/src/pages/register.tsx
+++ client/src/pages/register.tsx
@@ -6,6 +6,7 @@ import { useRegisterMutation } from '../generated/graphql';
 import { toErrorMap } from '../utils/toErrorMap';
 import Wrapper from '../components/Wrapper';
 import InputField from '../components/InputField';
+import Navbar from '../components/Navbar';
 
 interface Props {}
 
@@ -13,45 +14,48 @@ const Register: React.FC<Props> = ({}) => {
   const router = useRouter();
   const [, register] = useRegisterMutation();
   return (
-    <Wrapper size='small'>
-      <Formik
-        initialValues={{ username: '', password: '' }}
-        onSubmit={async (values, { setErrors }) => {
-          const response = await register(values);
-          if (response.data?.register.errors) {
-            setErrors(toErrorMap(response.data.register.errors));
-          } else if (response.data?.register.user) {
-            router.push('/');
-          }
-        }}
-      >
-        {({ isSubmitting }) => (
-          <Form>
-            <InputField
-              name='username'
-              placeholder='username'
-              label='Username'
-            />
-            <Box mt={4}>
+    <>
+      <Navbar />
+      <Wrapper size='small'>
+        <Formik
+          initialValues={{ username: '', password: '' }}
+          onSubmit={async (values, { setErrors }) => {
+            const response = await register(values);
+            if (response.data?.register.errors) {
+              setErrors(toErrorMap(response.data.register.errors));
+            } else if (response.data?.register.user) {
+              router.push('/');
+            }
+          }}
+        >
+          {({ isSubmitting }) => (
+            <Form>
               <InputField
-                name='password'
-                placeholder='password'
-                label='Password'
-                type={'password'}
+                name='username'
+                placeholder='username'
+                label='Username'
               />
-            </Box>
-            <Button
-              mt={4}
-              type='submit'
-              variantColor='teal'
-              isLoading={isSubmitting}
-            >
-              Register
-            </Button>
-          </Form>
-        )}
-      </Formik>
-    </Wrapper>
+              <Box mt={4}>
+                <InputField
+                  name='password'
+                  placeholder='password'
+                  label='Password'
+                  type={'password'}
+                />
+              </Box>
+              <Button
+                mt={4}
+                type='submit'
+                variantColor='teal'
+                isLoading={isSubmitting}
+              >
+                Register
+              </Button>
+            </Form>
+          )}
+        </Formik>
+      </Wrapper>
+    </>
   );
 };
 
