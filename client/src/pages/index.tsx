5a95b11b2fab8b74ff1e227d8bfbdb4c944a406d
Casper Feng <casp.feng@gmail.com>
2020-09-01 22:45:21 +0200

    i dependencies

--- client/src/pages/index.tsx
+++ client/src/pages/index.tsx
@@ -1,15 +1,20 @@
 import { withUrqlClient } from 'next-urql';
 import { createUrqlClient } from '../utils/createUrqlClient';
-import { usePostsQuery } from '../generated/graphql';
+import { usePostsQuery, Post } from '../generated/graphql';
 import Navbar from '../components/Navbar';
 
 const Index = () => {
+  const [{ data }] = usePostsQuery();
   return (
     <>
       <Navbar />
-      <div>Hello World</div>
+      Hello World!
+      <br />
+      {!data
+        ? null
+        : data.posts.map((post: Post) => <div key={post.id}>{post.title}</div>)}
     </>
   );
 };
 
-export default withUrqlClient(createUrqlClient)(Index);
+export default withUrqlClient(createUrqlClient, { ssr: true })(Index);
